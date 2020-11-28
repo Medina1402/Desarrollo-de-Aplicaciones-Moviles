a49ab019bded2ae5df1323912c2c3745ddef1920
Abraham Medina Carrillo <abraham.medina.carrillo@uabc.edu.mx>
2020-11-28 01:14:45 -0800

    Fragments UI Done

--- Bits and Pizzas/app/src/main/java/com/uabc/amc/bitsandpizzas/MainActivity.kt
+++ Bits and Pizzas/app/src/main/java/com/uabc/amc/bitsandpizzas/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.uabc.amc.bitsandpizzas
 
+import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.MenuItemCompat
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.ShareActionProvider
-import android.os.Bundle
-import android.support.v4.view.MenuItemCompat
+import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +24,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val toolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val pagerAdapter = SectionsPagerAdapter(supportFragmentManager, this)
+        val pager = findViewById<ViewPager>(R.id.pager)
+        pager.adapter = pagerAdapter
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+        tabLayout.setupWithViewPager(pager)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -26,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         val menuItem = menu?.findItem(R.id.action_share)
         sharedActionProvider = MenuItemCompat.getActionProvider(menuItem) as ShareActionProvider?
         setShareActionIntent("Want to join me for pizza?")
-
 
         return super.onCreateOptionsMenu(menu)
     }
@@ -43,9 +56,38 @@ class MainActivity : AppCompatActivity() {
             R.id.action_create_order -> {
                 val intent = Intent(this, OrderActivity::class.java)
                 startActivity(intent)
-                true
+                return true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+
+    private class SectionsPagerAdapter(fm: FragmentManager, val ctx: Context) : FragmentPagerAdapter(fm) {
+
+        override fun getCount(): Int {
+            return 4
+        }
+
+        override fun getItem(p0: Int): Fragment? {
+            return when(p0) {
+                0 -> TopFragment()
+                1 -> PizzaFragment()
+                2 -> PastaFragment()
+                3 -> StoresFragment()
+                else -> null
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position) {
+                0 -> ctx.getString(R.string.home_tab)
+                1 -> ctx.getString(R.string.pizza_tab)
+                2 -> ctx.getString(R.string.pasta_tab)
+                3 -> ctx.getString(R.string.store_tab)
+                else -> null
+            }
+        }
+
     }
 }
--- Bits and Pizzas/app/src/main/java/com/uabc/amc/bitsandpizzas/OrderActivity.kt
+++ Bits and Pizzas/app/src/main/java/com/uabc/amc/bitsandpizzas/OrderActivity.kt
@@ -1,17 +1,36 @@
 package com.uabc.amc.bitsandpizzas
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Toast
 
 class OrderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
 
-        val toolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.toolbar_main)
+        val toolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun onClickDone(view: View) {
+        Snackbar
+            .make(view, "Your order has been updated", Snackbar.LENGTH_SHORT)
+            .setAction("Undo", MySnackBarListener(this))
+            .show()
+    }
+
+    private class MySnackBarListener(val ctx: Context): View.OnClickListener {
+        override fun onClick(p0: View?) {
+            val toast = Toast.makeText(ctx, "Undone!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
